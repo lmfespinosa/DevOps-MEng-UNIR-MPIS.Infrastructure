fe740ae30bf886649168e2b8c62f2ab9e4b8504d
Luis Miguel Fernandez <luismiguel.fernandez@viseo.com>
2020-09-07 19:02:50 +0200

    Updated infrastructure.

--- src/azure-event-grid.tf
+++ src/azure-event-grid.tf
@@ -11,18 +11,18 @@ variable "topics" {
 
 # Resources
 
-# resource "azurerm_eventgrid_topic" "topic" {
-#   count                               = length(var.topics)
-#   name                                = "topic-${var.topics[count.index]}-${var.environment}"
-#   resource_group_name                 = azurerm_resource_group.resourcegroup.name
-#   location                            = var.azure_location
-#   tags                                = merge(var.azure_common_tags, {"service" = "event-grid-topic" })
-# }
+resource "azurerm_eventgrid_topic" "topic" {
+   count                               = length(var.topics)
+   name                                = "topic-${var.topics[count.index]}-${var.environment}"
+   resource_group_name                 = azurerm_resource_group.resourcegroup.name
+   location                            = var.azure_location
+   tags                                = merge(var.azure_common_tags, {"service" = "event-grid-topic" })
+}
 
-# resource "azurerm_eventgrid_topic" "topic_test" {
-#   count                               = length(var.topics)
-#   name                                = "topic-test-${var.topics[count.index]}-${var.environment}"
-#   resource_group_name                 = azurerm_resource_group.resourcegroup.name
-#   location                            = var.azure_location
-#   tags                                = merge(var.azure_common_tags, {"service" = "event-grid-topic" })
-# }
+resource "azurerm_eventgrid_topic" "topic_test" {
+  count                               = length(var.topics)
+  name                                = "topic-test-${var.topics[count.index]}-${var.environment}"
+  resource_group_name                 = azurerm_resource_group.resourcegroup.name
+  location                            = var.azure_location
+  tags                                = merge(var.azure_common_tags, {"service" = "event-grid-topic" })
+}
